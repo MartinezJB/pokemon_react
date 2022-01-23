9ce1c93b640c903781159276b0f39354c1ff612b
martinezjb <martinez.joaquin.b@gmail.com>
2022-01-23 13:41:16 -0300

    animacion a enemigo agregada, cambio en la plataforma del enemigo, sombras internas y externas en el container de info agregadas

--- src/components/EnemyDisplay.jsx
+++ src/components/EnemyDisplay.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 //import styles from './styles/EnemyDisplay.module.css';
-
+// TODO: hay que cambiar los width para que coincidan todos con columna 12. el module ya no sirve
 export default function EnemyDisplay({ enemy }) {
     let { name, lvl, max_life, actual_life, urlImg } = enemy;
 
     return (
         <div className="relative flex flex-row-reverse top-3 items-start justify-around">
             {/* Image */}
-            <div className="w-2/5">
+            <div className="w-5/12">
                 {/* Sprite */}
-                <img className="relative w-full z-10" src={urlImg} alt={name} />
+                <img className="relative w-full z-10 animate-bounce" src={urlImg} alt={name} /> {/* translate-y-[-15%] agregar esta clase si se desactivan las animaciones*/}
                 {/* Platform */}
-                <div className="absolute bottom-0 w-2/5 h-3/6 bg-slate-200 border-slate-400 border-4 rounded-full "></div>
+                <div className="absolute bottom-0 w-5/12 h-5/6 scale-y-50 bg-slate-200 border-slate-400 border-4 rounded-full "></div>
             </div>
 
             {/* Info */}
-            <div className="w-5/12 bg-gradient-to-br from-stone-500 to-stone-300 px-2 py-1 border-slate-800 border-2 rounded-r-lg">
+            <div className="w-5/12 bg-stone-500 px-2 py-1 border-slate-800 border-2 rounded-r-lg shadow-inner">
                 <div className="flex justify-between text-lg">
                     {/* Name */}
                     <p>{name}</p>
@@ -27,7 +27,7 @@ export default function EnemyDisplay({ enemy }) {
 
                 <div className="flex justify-end items-center">
                     <label className="mr-2 text-sm" htmlFor="enemyHealt">HP</label>
-                    <progress className="w-3/5 h-3 border-slate-800 border-2 rounded" id="enemyHealt" min="0" max={max_life} value={actual_life}></progress>
+                    <progress className="w-7/12 h-3 border-slate-800 border-2 rounded" id="enemyHealt" min="0" max={max_life} value={actual_life}></progress>
                 </div>
             </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'inner': 'inset 2px 2px 2px #AEB49E, inset -2px -2px 2px #AEB49E, 2px 2px 2px #E6F4E1, -2px -2px 2px #E6F4E1'
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%': { transform: 'translateY(-18%)' },
+          '50%': { transform: 'translateY(-15%)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
